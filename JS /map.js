d98a5244a114c85d0f437757bef5bee8b09535e7
Nour Al Safadi <nour.safadi7@icloud.com>
2024-01-15 23:57:57 +0100

    Initial commit

---
+++ JS /map.js
@@ -0,0 +1,76 @@
+// Initialize the map
+// document.addEventListener('DOMContentLoaded', function() {
+
+var map = L.map('map').setView([52.489415, 13.392281], 16.4);
+
+// Integrate Mapbox
+L.tileLayer('https://api.mapbox.com/styles/v1/nour-safadi/clrdolfht00ce01p409vphilw/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoibm91ci1zYWZhZGkiLCJhIjoiY2xwNnh5d2FnMXhsaTJqcWs4ZHFqNXM4aiJ9.cdEJMpU6paLl1t4Lk79MvA', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>',
+    minZoom:16,
+    maxZoom: 18,
+    tileSize: 512,
+    zoomOffset: -1,
+}).addTo(map);
+
+
+// Define locations 
+var locations = [
+    {
+        name: "Marheineke Markthalle",
+        coords: [52.48922871774201, 13.394424191124616],
+        label: "Historical",
+        imageUrl: "path-to-image-for-Marheineke.jpg" // Replace with actual image path
+    },
+    {
+        name: "Passionskirche",
+        coords: [52.48942454811764, 13.39713715986819],
+        label: "Historical",
+        imageUrl: "path-to-image-for-Passionskirche.jpg" // Replace with actual image path
+    }
+];
+
+// Create circles and add click event
+locations.forEach(function(location) {
+    var circle = L.circle(location.coords, {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 10
+    }).addTo(map);
+
+    circle.on('click', function() {
+        openLightbox(location);
+    });
+});
+
+// Function to open lightbox
+function openLightbox(location) {
+    var lightbox = document.querySelector('.lightbox');
+    var lightboxContent = document.querySelector('.lightbox-content');
+
+    lightboxContent.innerHTML = `
+        <img src="${location.imageUrl}" alt="${location.name}">
+        <p>${location.name} - ${location.label}</p>
+        <button onclick="closeLightbox()">X</button>
+    `;
+
+    lightbox.style.display = 'block';
+}
+
+// Function to close lightbox
+function closeLightbox() {
+    document.querySelector('.lightbox').style.display = 'none';
+}
+
+// Assuming initialLat, initialLng, and initialZoom are your map's initial view settings
+var initialLat =52.489415;
+var initialLng = 13.392281;
+var initialZoom = 16.4;
+
+
+function resetMapView() {
+    map.setView([initialLat, initialLng], initialZoom);
+}
+
+// Attach event listener to the button
+document.getElementById('resetMapView').addEventListener('click', resetMapView);
---
+++ JS /timeline.js
@@ -0,0 +1,5 @@
+
+  window.onload = function() {
+    // Replace 'your-google-spreadsheet-url' or 'your-json-file-url' with the URL of your data source
+    var timeline = new TL.Timeline('timeline-embed', 'https://docs.google.com/spreadsheets/d/1XcAzvD5oGUuEZnPXtp1Kuw8xhoHS3zgJU_9k2oNe7NQ/edit#gid=0');
+  };
